97c5cde371c1e1e7cc070547d141709063adad89
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2023-01-03 19:07:11 +0530

    Deleted credentials

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://VarunKashyap123:MRupV9bv4X3bETbY@cluster0.1gitkga.mongodb.net/?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
